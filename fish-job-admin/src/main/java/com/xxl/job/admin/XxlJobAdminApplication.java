6ca274c6150b477615343460f334c241ca259683
dayang <dayangtop@163.com>
2023-05-31 01:18:48 +0800

    add xxljob

---
+++ fish-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -0,0 +1,17 @@
+package com.xxl.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuxueli
+ * @since 2018-10-28
+ */
+@SpringBootApplication
+public class XxlJobAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxlJobAdminApplication.class, args);
+	}
+
+}
