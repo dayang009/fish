79ae7299ec30c7ab9f17579fdf6ff108745cabd5
dayang <dayangtop@163.com>
2024-10-15 22:45:14 +0800

    弃用bootstrap.yml形式的配置文件 添加工作流模块

---
+++ fish-flowable/src/main/java/com/fish/flowable/FlowableApplication.java
@@ -0,0 +1,16 @@
+package com.fish.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 工作流模块
+ */
+@SpringBootApplication
+public class FlowableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableApplication.class, args);
+	}
+
+}
